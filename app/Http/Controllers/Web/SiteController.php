6bd93c8693e02377df63d2acca8069cd0ff86f8b
root <root@Diego.localdomain>
2023-09-16 12:57:36 -0300

    First route

---
+++ app/Http/Controllers/Web/SiteController.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Http\Controllers\Web;
+
+class SiteController {
+    public function index() {
+        return view('web/contact');
+    }
+}
+
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Web\SiteController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,4 +16,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::controller(SiteController::class)->prefix('web')->group(function () {
+    Route::get('/contato', 'index');
 });
